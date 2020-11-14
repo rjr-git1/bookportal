cef74631b5ef5142ad1c3ee85a3b21983ca56a15
R Boury <raphaelboury@gmail.com>
2020-11-14 20:55:53 +0100

    new

--- dist/main.48b3db1d.js
+++ dist/main.48b3db1d.js
@@ -194,7 +194,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./../../node_modules/font-awesome/css/font-awesome.min.css":"../node_modules/font-awesome/css/font-awesome.min.css","_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./../../node_modules/font-awesome/css/font-awesome.min.css":"../node_modules/font-awesome/css/font-awesome.min.css","./../images/ficition_image_bg.jpg":[["ficition_image_bg.8a494bb2.jpg","images/ficition_image_bg.jpg"],"images/ficition_image_bg.jpg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -222,7 +222,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56355" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53493" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
